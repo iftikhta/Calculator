b3ac1be2938d40f5d4d4a4df63148a92093c22bd
Taha Iftikhar <iftikhta@shernet.sheridancollege.ca>
2019-01-28 17:44:34 -0500

    Only partial completion of task so far, saving for now
    Trying to come up with the backend Logic for basic operations. Instructions dont match windows default calcultor exactly but I am trying to follow them to the T, leading to some serious difficulty. I do not know how to parse a string and split parts of it into integers.

--- Assignment1Calculator/MainPage.xaml.cs
+++ Assignment1Calculator/MainPage.xaml.cs
@@ -20,13 +20,18 @@ namespace Assignment1Calculator
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        String status = "";
+        String temp = "";
+
         public MainPage()
         {
             this.InitializeComponent();
+            
         }
-
+        /*
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -41,10 +46,161 @@ namespace Assignment1Calculator
         {
 
         }
-
+        */
         private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            //string newVar = userInp.Parse(userInp.Text); This does not work, took me a an embarssing amount of time to get this right
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "1";
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "2";
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "3";
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "4";
+
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "5";
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "6";
+
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "7";
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "8";
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "9";
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            if (status != "")
+            {
+                headInp.Text = temp;
+                userInp.Text = "";
+                status = "";
+            }
+            userInp.Text = userInp.Text + "0";
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            temp = userInp.Text + '/';
+            status = "/";
+
+        }
+
+        private void FullClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+            temp = userInp.Text + '-';
+            status = "-";
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            temp = userInp.Text + '*';
+            status = "*";
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            temp = userInp.Text + '+';
+            status = "+";
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            headInp.Text = headInp.Text + userInp.Text;
+            String Total = "Placeholder";
+            userInp.Text = Total;
+            status = "=";
         }
     }
 }
